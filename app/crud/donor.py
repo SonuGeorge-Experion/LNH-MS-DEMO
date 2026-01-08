18d5f52fa70c942a7a93ff0cda10a03d3ca238ae
advait3368 <advait.kumar@experionglobal.com>
2026-01-08 17:02:07 +0530

    get and update api added

--- app/crud/donor.py
+++ app/crud/donor.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.asyncio import AsyncSession
-
+from sqlalchemy import select
 from app.db.models.donor import Donors
 from app.schemas.donor import DonorSchema
 
@@ -19,3 +19,23 @@ async def add_donor(request: DonorSchema, db: AsyncSession):
     await db.commit()
     await db.refresh(new_donor)
     return new_donor
+
+async def get_donors(db: AsyncSession,skip: int, limit: int):
+    query = select(Donors).offset(skip).limit(limit)
+    result = await db.execute(query)
+    return result.scalars().all()
+
+async def update_donor(db: AsyncSession, donor_id: int, donor_in: DonorSchema):
+    query = select(Donors).where(Donors.donor_id == donor_id)
+    result = await db.execute(query)
+    db_donor = result.scalar_one_or_none()
+
+    if db_donor:
+        update_data = donor_in.model_dump()
+        for key, value in update_data.items():
+            setattr(db_donor, key, value)
+        
+        await db.commit()
+        await db.refresh(db_donor)
+    
+    return db_donor
